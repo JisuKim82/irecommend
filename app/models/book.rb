ca0606339ceab5614833444946c16305c6959c9c
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-17 21:17:04 -0400

    Add Book class scope methods(lambdas)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,16 @@
 class Book < ActiveRecord::Base
   has_many :book_genres
   has_many :genres, through: :book_genres
+
+  scope :finished, ->{ where('finished_on IS NOT NULL') }
+  scope :recent, ->{ where('finished_on > ?', 10.days.ago) }
+  
+  ### replaced by "scope :recent, ->{...}"
+  # def self.recent
+  #   self.where('finished_on > ?', 10.days.ago)  
+  # end
+
+  def finished?
+    finished_on.present?
+  end
 end
