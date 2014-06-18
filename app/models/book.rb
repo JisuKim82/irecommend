5d819fbfd3a7e0f349df19c179ec1f4061a3aec0
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-17 22:52:27 -0400

    Make Search Advanced - case insensitive, can search based on author, title, description

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,15 +2,19 @@ class Book < ActiveRecord::Base
   has_many :book_genres
   has_many :genres, through: :book_genres
 
+  before_save :set_keywords
+
   scope :finished, ->{ where('finished_on IS NOT NULL') }
   scope :recent, ->{ where('finished_on > ?', 10.days.ago) }
-  scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
+  scope :search, ->(keyword){ where('keywords LIKE ?',"%#{keyword.downcase}%") if keyword.present? }
   
   ### replaced by "scope :recent, ->{...}"
   # def self.recent
   #   self.where('finished_on > ?', 10.days.ago)  
   # end
-
+  
+  #scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
+  
   ### replaced by "scope :search, ->(keyword){...}"
   # def self.search(keyword)
   #   if keyword.present?
@@ -24,4 +28,8 @@ class Book < ActiveRecord::Base
     finished_on.present?
   end
 
+  protected
+    def set_keywords
+      self.keywords = [title, author, description].map(&:downcase).join(' ')
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617165445) do
+ActiveRecord::Schema.define(version: 20140618023548) do
 
   create_table "book_genres", force: true do |t|
     t.integer  "book_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140617165445) do
     t.string   "finished_on"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "keywords"
   end
 
   create_table "genres", force: true do |t|
