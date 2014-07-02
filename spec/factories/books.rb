67d51f30a1a81b29ff67c0f162e371dd6b294d0d
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-01 21:37:56 -0400

    Add Books Factory

---
+++ spec/factories/books.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do 
+  factory :book do
+    title "How to use FactoryGirl"
+    author "Jisu Kim"
+    description "Lorme Ipsum Kf Factory"
+    amazon_id "0987654321"
+    rating 3
+  end
+
+  factory :genre do
+    name 'Programming'
+  end
+
+  factory :bookgenre do
+    book
+    genre
+  end
+  
+end
