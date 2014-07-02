b47e66efc412be515912955768952b83a6ac340d
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-02 15:48:50 -0400

    Add pending tests

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -4,6 +4,42 @@ describe BooksController do
   describe 'GET #index' do
     context 'with params[:keyword]' do
       it 'populates an array of Book objects matching the keyword'
+      it 'renders the :index template'
+    end
+
+    context 'without a params[:keyword]' do
+      it 'populates an array of all Book objects'
+      it 'renders the :index template'
     end
   end
+
+  describe 'GET #edit' do
+    it 'assigns the requested book to @book'
+    it 'renders the :edit template'
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new book in the database'
+      it 'redirects to books#index'
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the book to the database'
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'updates the book in the database'
+      it 'redirects to books#index'
+    end
+
+    context 'with invalid attributes' do
+      it 'does not update the book'
+      it 're-renders the :edit template'
+    end
+  end
+
+
 end
