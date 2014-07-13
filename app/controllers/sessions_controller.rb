d45eeca987041aea12270b23fbebf39d0f031576
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-13 11:47:58 -0400

    Update routes and sessions_ontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 class SessionsController < ApplicationController
-  def login
-  end
 
   def new
   end
 
   def create
     @user = User.find_by(email: params[:email])
-    if @user
-      session[:user_id] = @user.id
+    if @user #&& valid_user?
+      login(@user)
       redirect_to "/"
     else
       render :login
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to '/', :notice => "You're logged out"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :books
   root "books#index"
 
-  get '/login' => "sessions#login"
-  get '/new' => "sessions#new"
+  get '/login' => "sessions#new"
+  get '/logout' => "sessions#destroy"
   post '/sessions' => "sessions#create"
 end
