d458be7306608e13bfdd00d9f31cf1b66a2d2bde
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-13 11:44:30 -0400

    Add additional login methods and make them helper_method to access in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   private
+  def login_required
+    if !logged_in?
+      redirect_to '/login', :notice => "Please Login"
+    end
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
   def logged_in?
     !!session[:user_id]
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
