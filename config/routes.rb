57616b97c237160669e95733d2a391b700116def
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-13 12:27:52 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get '/login' => "sessions#new"
   get '/logout' => "sessions#destroy"
   post '/sessions' => "sessions#create"
+
+  get '/register' => "users#new", as: 'users'
+  post '/register' => "users#create" 
 end
